c9b57bee0a7fdb3863770c60f2235cec7eee696f
xkayser1997 <xkayser1997@gmail.com>
2026-07-23 11:48:15 -0500

    Update summary_tables.sql

--- summary_tables.sql
+++ summary_tables.sql
@@ -1,5 +1,18 @@
 --Creation of Summary Tables for export to Tableau
 
+--Overview of KPIs
+CREATE OR REPLACE TABLE `cedar-turbine-501913-v0.lend_club.accepted_KPIs`
+AS
+SELECT
+  COUNT(*) AS total_loans,
+  SUM(funded_amnt) AS total_funded,
+  SUM(total_pymnt) AS total_received,
+  AVG(loan_amnt) AS avg_loan_amount,
+  AVG(int_rate) AS avg_interest,
+  AVG(dti) AS avg_dti,
+  AVG(annual_inc) AS avg_annual_inc
+FROM `cedar-turbine-501913-v0.lend_club.accepted_clean_final`
+  
 --Yearly trends
 CREATE OR REPLACE TABLE`cedar-turbine-501913-v0.lend_club.accepted_yearly_overview`
 AS
