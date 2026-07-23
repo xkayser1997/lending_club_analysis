9006a2e83a6f03665f7a57f09108d6c3eccd0fcd
xkayser1997 <xkayser1997@gmail.com>
2026-07-23 10:33:04 -0500

    Update summary_tables.sql

--- summary_tables.sql
+++ summary_tables.sql
@@ -13,3 +13,16 @@ SELECT
 FROM `cedar-turbine-501913-v0.lend_club.accepted_clean_final`
 GROUP BY issue_year
 ORDER BY issue_year
+
+--State performance
+CREATE OR REPLACE TABLE `cedar-turbine-501913-v0.lend_club.accepted_state_summary`
+AS
+SELECT
+  addr_state,
+  COUNT(*) AS total_loans,
+  SUM(funded_amnt) as total_funded,
+  AVG(int_rate) as avg_interest,
+  AVG(dti) as avg_dti
+FROM `cedar-turbine-501913-v0.lend_club.accepted_clean_final`
+GROUP BY addr_state
+ORDER BY addr_state;
